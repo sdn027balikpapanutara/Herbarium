17097907c63ae562da29fc00fc7dbc8eb8a051e4
sdn027balikpapanutara <sdn027balikpapanutara1@gmail.com>
2026-09-13 15:40:07 +0800

    Edit

--- assets/js/config.js
+++ assets/js/config.js
@@ -16,7 +16,7 @@ const CONFIG = {
      Format: https://USERNAME-GITHUB.github.io/NAMA-REPO/
      Contoh : https://budi-herbarium.github.io/herbarium-web/
      Dipakai untuk membuat URL yang dikodekan ke dalam QR Code. */
-  BASE_URL: "https://USERNAME-GITHUB.github.io/NAMA-REPO/",
+  BASE_URL: "https://github.com/sdn027balikpapanutara/Herbarium",
 
   /* ⚠️ WAJIB DIISI #2 — ID GOOGLE SPREADSHEET
      Ambil dari URL spreadsheet:
